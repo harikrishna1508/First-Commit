16b66a543dcbe40b57209a1bb2000a0b3f3d7a37
harikrishna1508 <pharikrishna1508@gmail.com>
2021-10-06 15:00:14 +0530

    second commit

--- src/UsersComponent.jsx
+++ src/UsersComponent.jsx
@@ -23,10 +23,10 @@ const Users = () => {
   return (
     <>
       {users.length > 0 && users.map((user) => ( 
-        <Card key={user.id} user={user} /> 
+        <Card key={user.id} user={user} />  
       ))}
       {users.length === 0 && <p>No users available!</p>} 
-    </>
+    </> 
   )
 }
 
